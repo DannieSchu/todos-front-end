8601b610c11e78f699542798e5822db3405460d1
Dannie Schumaker <draeschumaker@gmail.com>
2020-02-29 17:57:02 -0800

    add private route component to redirect user to signin page

--- src/App.js
+++ src/App.js
@@ -5,18 +5,23 @@ import TodoList from './Components/TodoList';
 import SignUp from './Components/SignUp';
 import SignIn from './Components/SignIn';
 import Footer from './Components/Footer';
+import PrivateRoute from './Components/PrivateRoute';
 import './App.css';
 
 export default class App extends Component {
+  state = { user: null };
+  setUser = (user) => {
+    this.setState({user: user.body})
+  }
   render() {  
     return (
       <div> 
         <Header />
         <Router>
           <Switch>
-            <Route path = "/signin" component = {SignIn} />
-            <Route path = "/signup" component = {SignUp} />
-            <Route path = "/" component = {TodoList} />
+            <Route path = "/signin" component = {SignIn} render={(props) => <Login {...props} setUser={ this.setUser } user={this.state.user }/>} />
+            <Route path = "/signup" component = {SignUp} render={(props) => <Login {...props} setUser={ this.setUser } user={this.state.user }/>} />
+            <PrivateRoute path = "/" component = {TodoList} user={this.state.user} />
           </Switch>
         </Router>
         <Footer />
